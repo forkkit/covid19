2414ea252257b4629374dd3896d6475fb31bf0e7
Amnon BC <amnonbc@gmail.com>
2020-03-15 14:35:12 +0000

    parse WHO data Use collumn headers to idenitify columns More tests

--- who/covid.go
+++ who/covid.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"strconv"
+	"strings"
 	"time"
 )
 
@@ -20,12 +21,36 @@ type rawData struct {
 	totalDeaths int
 }
 
-func atoi(s string) (int, error) {
+func getField(rec []string, headers []string, item string) (string, error) {
+	i := 0
+	h := ""
+	for i, h = range headers {
+		if h == item {
+			break
+		}
+	}
+	if h != item {
+		return "", fmt.Errorf("No such column %q in CSV steam", item)
+	}
+	if len(rec) <= i {
+		return "", fmt.Errorf("Too few fields in record")
+	}
+	return strings.TrimSpace(rec[i]), nil
+}
+
+func getIntField(rec []string, headers []string, item string) (int, error) {
+	s, err := getField(rec, headers, item)
+	if err != nil {
+		return 0, err
+	}
 	if s == "" {
 		return 0, nil
 	}
-	return strconv.Atoi(s)
-
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		return 0, fmt.Errorf("Error parsing CSV stream: bad format for %s, could not parse %q as an integer", item, s)
+	}
+	return i, nil
 }
 
 func ReadFullData(in io.Reader) ([]rawData, error) {
@@ -50,30 +75,39 @@ func ReadFullData(in io.Reader) ([]rawData, error) {
 		if err != nil {
 			return out, err
 		}
-		date, err := time.Parse("2006-01-02", rec[0])
+		tf, err := getField(rec, headers, "date")
 		if err != nil {
 			return out, err
 		}
-		newCases, err := atoi(rec[2])
+		date, err := time.Parse("2006-01-02", tf)
+		if err != nil {
+			return out, fmt.Errorf("Could not parse date in CSV stream, %w", err)
+		}
+		location, err := getField(rec, headers, "location")
 		if err != nil {
 			return out, err
 		}
-		newDeaths, err := atoi(rec[3])
+
+		newCases, err := getIntField(rec, headers, "new_cases")
 		if err != nil {
 			return out, err
 		}
-		totalCases, err := atoi(rec[4])
+		newDeaths, err := getIntField(rec, headers, "new_deaths")
 		if err != nil {
 			return out, err
 		}
-		totalDeaths, err := atoi(rec[5])
+		totalCases, err := getIntField(rec, headers, "total_cases")
+		if err != nil {
+			return out, err
+		}
+		totalDeaths, err := getIntField(rec, headers, "total_deaths")
 		if err != nil {
 			return out, err
 		}
 
 		out = append(out, rawData{
 			date,
-			rec[1],
+			location,
 			newCases,
 			newDeaths,
 			totalCases,
--- who/covid_test.go
+++ who/covid_test.go
@@ -35,12 +35,11 @@ func Test_readFullDataOne(t *testing.T) {
 
 func Test_readFullData(t *testing.T) {
 	testData := `date,location,new_cases,new_deaths,total_cases,total_deaths
-2020-03-09,United Kingdom,67,0,277,2
-2020-03-10,United Kingdom,46,1,323,3
-2020-03-11,United Kingdom,50,3,373,6
-2020-03-12,United Kingdom,87,0,460,6
-2020-03-13,United Kingdom,134,2,594,8
-`
+		2020-03-09,United Kingdom,67,0,277,2
+		2020-03-10,United Kingdom,46,1,323,3
+		2020-03-11,United Kingdom,50,3,373,6
+		2020-03-12,United Kingdom,87,0,460,6
+		2020-03-13,United Kingdom,134,2,594,8`
 	o, err := ReadFullData(strings.NewReader(testData))
 	require.NoError(t, err)
 	require.Equal(t, 5, len(o))
@@ -57,4 +56,24 @@ func Test_readFullData(t *testing.T) {
 	assert.Equal(t, 594, o[4].totalCases)
 	assert.Equal(t, 8, o[4].totalDeaths)
 	assert.Equal(t, "United Kingdom", o[4].location)
+}
+
+func Test_readFullError(t *testing.T) {
+	testData := `date,xxx,new_cases,new_deaths,total_cases,total_deaths
+		2020-02-25,Afghanistan,1,2,3,4`
+	_, err := ReadFullData(strings.NewReader(testData))
+	assert.Error(t, err)
+}
+
+func Test_readFullErrorBadDate(t *testing.T) {
+	testData := `date,location,new_cases,new_deaths,total_cases,total_deaths
+		abc,Afghanistan,1,2,3,4`
+	_, err := ReadFullData(strings.NewReader(testData))
+	assert.Error(t, err)
+}
+
+func Test_readFullNotCSV(t *testing.T) {
+	testData := `"`
+	_, err := ReadFullData(strings.NewReader(testData))
+	assert.Error(t, err)
 }
