c8c1e770c3df0ff8fdf07fafba6f353936f324c3
Amnon BC <amnonbc@gmail.com>
2020-03-17 18:10:51 +0000

    use gorrilla middleware logger

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/amnonbc/covid19
 
 go 1.14
 
-require github.com/stretchr/testify v1.5.1
+require (
+	github.com/gorilla/handlers v1.4.2
+	github.com/stretchr/testify v1.5.1
+)
--- webapp.go
+++ webapp.go
@@ -3,9 +3,11 @@ package main
 import (
 	"encoding/json"
 	"github.com/amnonbc/covid19/who"
+	"github.com/gorilla/handlers"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"sync"
 	"time"
 )
@@ -17,7 +19,6 @@ var lock sync.RWMutex
 var whoStats []who.RawData
 
 func homepage(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr, r.Method, r.URL)
 	tmpl.Execute(w, nil)
 }
 
@@ -76,14 +77,14 @@ func updater() {
 
 func main() {
 	go updater()
-	http.HandleFunc("/", homepage)
-	http.HandleFunc("/stats.json", stats)
-	http.HandleFunc("/country.json", country)
-	http.HandleFunc("/country.html", countryPage)
-	http.HandleFunc("/favicon.ico", favicon)
-
-	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-	log.Println("listening on localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := http.NewServeMux()
+	r.HandleFunc("/", homepage)
+	r.HandleFunc("/stats.json", stats)
+	r.HandleFunc("/country.json", country)
+	r.HandleFunc("/country.html", countryPage)
+	r.HandleFunc("/favicon.ico", favicon)
+	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	log.Println("listening on http://localhost:8080/")
+	log.Fatal(http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, r)))
 
 }
